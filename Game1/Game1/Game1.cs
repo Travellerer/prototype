51dff39d70e7763962b2aabf96f6d09c1cc6cd00
Travellerer <kilianpoessel@googlemail.com>
2016-04-19 14:42:54 +0200

    Kamera

--- Game1/Game1/Game1.cs
+++ Game1/Game1/Game1.cs
@@ -12,6 +12,24 @@ namespace Game1
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        //Camera
+        Vector3 camTarget;
+        Vector3 camPosition;
+        Matrix projectionMatrix;
+        Matrix viewMatrix;
+        Matrix worldMatrix;
+
+        //Orbit
+        bool orbit = false;
+
+
+        //BasicEffect for rendering
+        BasicEffect basicEffect;
+
+        //Geometric info
+        VertexPositionColor[] triangleVertices;
+        VertexBuffer vertexBuffer;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -28,8 +46,49 @@ namespace Game1
         {
             // TODO: Add your initialization logic here
 
+            //Setup Camera
+            camTarget = new Vector3(0f, 0f, 0f);
+            camPosition = new Vector3(0f, 0f, -100f);
+            projectionMatrix = Matrix.CreatePerspectiveFieldOfView(
+                               MathHelper.ToRadians(45f),
+                               GraphicsDevice.DisplayMode.AspectRatio,
+                1f, 1000f);
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         new Vector3(0f, 1f, 0f));// Y up
+            worldMatrix = Matrix.CreateWorld(camTarget, Vector3.
+                          Forward, Vector3.Up);
+
+
             base.Initialize();
             IsMouseVisible = true;
+
+
+            //BasicEffect
+            basicEffect = new BasicEffect(GraphicsDevice);
+            basicEffect.Alpha = 1f;
+
+            // Want to see the colors of the vertices, this needs to be on
+            basicEffect.VertexColorEnabled = true;
+
+            //Lighting requires normal information which       VertexPositionColor does not have
+            //If you want to use lighting and VPC you need to create a       custom def
+            basicEffect.LightingEnabled = false;
+
+            //Geometry  - a simple triangle about the origin
+            triangleVertices = new VertexPositionColor[3];
+            triangleVertices[0] = new VertexPositionColor(new Vector3(
+                                  0, 20, 0), Color.Red);
+            triangleVertices[1] = new VertexPositionColor(new Vector3(-
+                                  20, -20, 0), Color.Green);
+            triangleVertices[2] = new VertexPositionColor(new Vector3(
+                                  20, -20, 0), Color.Blue);
+
+            //Vert buffer
+            vertexBuffer = new VertexBuffer(GraphicsDevice, typeof(
+                           VertexPositionColor), 3, BufferUsage.
+                           WriteOnly);
+            vertexBuffer.SetData<VertexPositionColor>(triangleVertices)
+                                                      ;
         }
 
         /// <summary>
@@ -60,8 +119,56 @@ namespace Game1
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            if (Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
+
+            if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(
+                                                       MathHelper.ToRadians(-1f));
+                camPosition = Vector3.Transform(camPosition,
+                              rotationMatrix);
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(
+                                       MathHelper.ToRadians(1f));
+                camPosition = Vector3.Transform(camPosition,
+                              rotationMatrix);
+            }
+            // Position und Target ersetzen mit abhängig von Modell
+            //if (Keyboard.GetState().IsKeyDown(Keys.Up))
+            //{
+            //    camPosition.Y -= 1f;
+            //    camTarget.Y -= 1f;
+            //}
+            //if (Keyboard.GetState().IsKeyDown(Keys.Down))
+            //{
+            //    camPosition.Y += 1f;
+            //    camTarget.Y += 1f;
+            //}
+            if (Keyboard.GetState().IsKeyDown(Keys.OemPlus))
+            {
+                camPosition.Z += 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.OemMinus))
+            {
+                camPosition.Z -= 1f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space))
+            {
+                orbit = !orbit;
+            }
+
+            if (orbit)
+            {
+                Matrix rotationMatrix = Matrix.CreateRotationY(
+                                        MathHelper.ToRadians(1f));
+                camPosition = Vector3.Transform(camPosition,
+                              rotationMatrix);
+            }
+            viewMatrix = Matrix.CreateLookAt(camPosition, camTarget,
+                         Vector3.Up);
 
             // TODO: Add your update logic here
 
@@ -77,6 +184,26 @@ namespace Game1
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
+
+            basicEffect.Projection = projectionMatrix;
+            basicEffect.View = viewMatrix;
+            basicEffect.World = worldMatrix;
+
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+            GraphicsDevice.SetVertexBuffer(vertexBuffer);
+
+            //Turn off culling so we see both sides of our rendered          triangle
+            RasterizerState rasterizerState = new RasterizerState();
+            rasterizerState.CullMode = CullMode.None;
+            GraphicsDevice.RasterizerState = rasterizerState;
+
+            foreach (EffectPass pass in basicEffect.CurrentTechnique.
+                    Passes)
+            {
+                pass.Apply();
+                GraphicsDevice.DrawPrimitives(PrimitiveType.
+                                              TriangleList, 0, 3);
+            }
 
             base.Draw(gameTime);
         }
